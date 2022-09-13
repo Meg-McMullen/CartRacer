bf07a8270561437cca55539426b2ac1e696dc808
Meg-McMullen <mcmullenmeggie@gmail.com>
2022-09-13 16:17:57 -0400

    Added Teleporter

---
+++ mcmullen_CartRacer1/Assets/Scripts/Teleporter1.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter1 : MonoBehaviour
+{
+    public GameObject tpPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider Col) {
+        if (Col.gameObject.tag == "Player")
+            Col.gameObject.transform.position = tpPoint.transform.position;
+        //Col.transform.position = new Vector3 (18,1,30);
+    }
+}
